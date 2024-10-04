19c0338bd682a91dde4130781957e79cf7fc4fac
Satyam Mohanty <111577463+Satyam173@users.noreply.github.com>
2024-10-05 00:54:52 +0530

    use profile hook

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { Avatar, AvatarImage } from './ui/avatar'
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import useGetUserProfile from "@/hooks/useGetUserProfile";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const params = useParams();
+  const userId = params.id;
+  useGetUserProfile();
+  const { userProfile } = useSelector((store) => store.auth);
   return (
-    <div>
-      <Avatar>
-        <AvatarImage/>
-      </Avatar>
+    <div className="flex max-w-4xl justify-center mx-auto pl-10">
+      <div className="flex flex-col gap-20 p-8">
+        <div className="grid grid-col-2">
+          <section className="flex items-center justify-center">
+            <Avatar className="h-32 w-32">
+              <AvatarImage
+                src={userProfile?.profilePicture}
+                alt="profile picture"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </section>
+          <section>
+            <div className="flex flex-col gap-5">
+              <span>{userProfile?.username}</span>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- frontend/src/hooks/useGetUserProfile.jsx
+++ frontend/src/hooks/useGetUserProfile.jsx
@@ -1,17 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
-import { setSuggestedUsers } from "@/redux/authSlice";
+import { setUserProfile } from "@/redux/authSlice";
 
-const useGetUserProfile = () => {
-    const dispatch = useDispatch()
+const useGetUserProfile = (userId) => {
+    const dispatch = useDispatch();
+    // const [userProfile,setUserProfile] = useState(null);
     useEffect(()=>{
         const fetchUserProfile  = async ()=>{
             try{
-                const res = await axios.get('http://localhost:8000/api/v1/user/suggested', {withCredentials:true});
+                const res = await axios.get(`http://localhost:8000/api/v1/user/${userId}/profile`, {withCredentials:true});
                 if(res.data.success){
                     // console.log(res.data.posts);
-                    dispatch(setSuggestedUsers(res.data.users))
+                    dispatch(setUserProfile(res.data.users))
                 }
             }
        catch(err){
@@ -19,7 +20,7 @@ const useGetUserProfile = () => {
         }
     }
     fetchUserProfile();
-    },[])
+    },[userId])
 }
 
 export default useGetUserProfile;
